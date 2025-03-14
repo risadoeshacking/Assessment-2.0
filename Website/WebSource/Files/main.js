ced3dd6576d0f7dc91224c89fb2d0de773a394f9
risadoeshacking <25235@rosmini.school.nz>
2025-03-14 19:59:50 +1300

    changes

--- Website/WebSource/Files/main.js
+++ Website/WebSource/Files/main.js
@@ -2,16 +2,11 @@ const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 const menuBtnIcon = menuBtn.querySelector("i");
 
-menuBtn.addEventListener("click", () => {
+menuBtn.addEventListener("click", (e) => {
   navLinks.classList.toggle("open");
 
-  if (navLinks.classList.contains("open")) {
-    menuBtnIcon.classList.remove("ri-menu-line");
-    menuBtnIcon.classList.add("ri-close-line");
-  } else {
-    menuBtnIcon.classList.remove("ri-close-line");
-    menuBtnIcon.classList.add("ri-menu-line");
-  }
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
 });
 
 navLinks.addEventListener("click", (e) => {
@@ -19,17 +14,63 @@ navLinks.addEventListener("click", (e) => {
   menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
 
-const scrollRevealoption = {
+const scrollRevealOption = {
   origin: "bottom",
   distance: "50px",
   duration: 1000,
 };
 
-scrollReveal().reveal(".header_image img",{
-  ...scrollRevealoption,
+ScrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
   origin: "right",
 });
-scrollReveal().reveal(".header_content p",{
-  ...scrollRevealoption,
+ScrollReveal().reveal(".header__content p", {
+  ...scrollRevealOption,
   delay: 500,
+});
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".header__btns", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".destination__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".showcase__image img", {
+  ...scrollRevealOption,
+  origin: "left",
+});
+ScrollReveal().reveal(".showcase__content h4", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".showcase__content p", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".showcase__btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".banner__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".discover__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
 });
